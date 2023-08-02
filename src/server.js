a52b60ee75f68563d42eec5dbcf28d4032560037
root <root@Luntik>
2023-08-02 20:06:39 +0500

    hello

--- src/server.js
+++ src/server.js
@@ -1,19 +1,18 @@
 import "dotenv/config";
 import express from "express";
 
+const smth = "";
 
 async function bootstrapt() {
-    try {
+  try {
+    const app = express();
 
-        const app = express();
-
-        app.listen(process.env["PORT"] || 5000, process.env['HOST'], () => console.log('Server is running...'));
-
-    } catch (error) {
-
-        console.log(error.message);
-        
-    }
+    app.listen(process.env["PORT"] || 5000, process.env["HOST"], () =>
+      console.log("Server is running...")
+    );
+  } catch (error) {
+    console.log(error.message);
+  }
 }
 
-bootstrapt()
+bootstrapt();
