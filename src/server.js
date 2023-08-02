112803e66158f1ebc63f10c45e7d6ff914b942af
eugene <umid09tuxtayev@gmail.com>
2023-08-02 19:49:07 +0500

    enter added

--- src/server.js
+++ src/server.js
@@ -4,11 +4,15 @@ import express from "express";
 
 async function bootstrapt() {
     try {
+
         const app = express();
 
         app.listen(process.env["PORT"] || 5000, process.env['HOST'], () => console.log('Server is running...'));
+
     } catch (error) {
+
         console.log(error.message);
+        
     }
 }
 
