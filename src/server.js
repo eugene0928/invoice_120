443fa02bad7b4059b91f6fb0f79a8e9326e17f08
DiyorbekWebs <github1805@gmail.com>
2023-08-02 19:54:10 +0500

    server edit

--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@ import express from "express";
 async function bootstrapt() {
     try {
         const app = express();
-
+// console.log(1);
         app.listen(process.env["PORT"] || 5000, process.env['HOST'], () => console.log('Server is running...'));
     } catch (error) {
         console.log(error.message);
