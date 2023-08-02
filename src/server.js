8096dee8cdf8b8a4be6d3a89bd594d0cb245d213
root <root@Luntik>
2023-08-02 20:23:19 +0500

    smth

--- src/server.js
+++ src/server.js
@@ -1,11 +1,12 @@
 import "dotenv/config";
-import express from "express";
-
-const smth = "";
+import express, { urlencoded } from "express";
 
 async function bootstrapt() {
   try {
     const app = express();
+
+    app.use(urlencoded({ extended: true }));
+    app.use(express.json());
 
     app.listen(process.env["PORT"] || 5000, process.env["HOST"], () =>
       console.log("Server is running...")
