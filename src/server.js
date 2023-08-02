ec95a3db998244d0dffd68fef0ef3f3811e1d4a6
Mahmud <rahmatjonovmahmudjonn@gmail.com>
2023-08-02 20:31:41 +0500

    checking pull request

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ import express from "express";
 
 async function bootstrapt() {
     try {
-
+        //checking for pull request from mahmud
         const app = express();
 
         app.listen(process.env["PORT"] || 5000, process.env['HOST'], () => console.log('Server is running...'));
