6113ecac2362b2406614386ec002828efab64bf0
eugene <umid09tuxtayev@gmail.com>
2023-08-02 19:25:06 +0500

    init server

---
+++ src/server.js
@@ -0,0 +1,15 @@
+import "dotenv/config";
+import express from "express";
+
+
+async function bootstrapt() {
+    try {
+        const app = express();
+
+        app.listen(process.env["PORT"] || 5000, process.env['HOST'], () => console.log('Server is running'));
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
+bootstrapt()
